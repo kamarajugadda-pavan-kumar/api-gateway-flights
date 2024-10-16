9c41ff7f0c20a2fac5a7ffe58fce36fc922c2ae9
pavan kumar kamarajugadda <pavankumarkamarajugadda@pavans-MacBook-Pro.local>
2024-10-16 07:41:31 +0530

    cors added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const rateLimit = require("express-rate-limit");
 const { createProxyMiddleware } = require("http-proxy-middleware");
-
+const cors = require("cors");
 
 const { ServerConfig } = require("./config");
 const apiRoutes = require("./routes");
 
 const app = express();
+
+// =============================================================================
+// Enable CORS (Cross-Origin Resource Sharing)
+// =============================================================================
+app.use(cors()); // Enable CORS for all routes
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
